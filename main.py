e28c819e687efe7befa6f76ae9c5d4add23fc32f
Adrian Rosing <97479464+LastEternity@users.noreply.github.com>
2026-03-30 16:12:36 -0700

    Remove unused server IDs from main.py
    Removed SERVER2_ID and SERVER3_ID from guilds list.

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ from utils.realmshark_notifier import build_realmshark_notifier
 from utils.autocomplete import class_autocomplete, item_name_autocomplete, bonus_autocomplete, user_bonus_autocomplete, target_user_bonus_autocomplete, target_user_ppe_id_autocomplete, team_name_autocomplete
 
 SERVER1_ID = 879497062117412924 # Last Oasis
-SERVER2_ID = 1435436110829326459 # Test Server
-SERVER3_ID = 1485395885666992248 # My Testing Server
 
 guilds = [discord.Object(id=SERVER1_ID), discord.Object(id=SERVER2_ID), discord.Object(id=SERVER3_ID)]
 
